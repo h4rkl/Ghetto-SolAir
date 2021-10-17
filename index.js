124bd8754b3e5e3770c056568ef0a7d3e3c36316
Harkl <3zy3@protonmail.com>
2021-10-18 11:51:50 +1300

    csv to kv json

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
+const fs = require("fs");
+const _ = require("lodash");
 const Spreadsheet = require("@moreavy/simple-csv-tools");
 
 const csvToJSON = () => {
-    const sheet = new Spreadsheet("data/raw.csv");
-    sheet.writeJSON("data/raw.json")
+  const sheet = new Spreadsheet("data/raw.csv");
+  const data = sheet.toArr("data/raw.json");
+  // sheet.parse(data);
+  const head = ['timestamp','handle','address'];
+  const kvData = [];
+  _.forEach(data, (value) => {
+    var result = {};
+    for (var i = 0; i < head.length; i++) {
+      result[head[i]] = value[i];
+    }
+    kvData.push(result);
+  });
+  fs.appendFile('data/cleaned.json', JSON.stringify(kvData), function (err) {
+    if (err) return console.log(err);
+    console.log('Cleaned');
+  });
 };
 
+const dedupeJSONKey = (key) => {
+  var data = require('./data/raw.json');
+  data.forEach(d => {
+
+  });
+  fs.appendFile('data/cleaned.json', JSON.stringify(data), function (err) {
+    if (err) return console.log(err);
+    console.log('Cleaned');
+  });
+}
+
 csvToJSON();
+// dedupeJSONKey();
